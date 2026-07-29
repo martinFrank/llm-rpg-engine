28459e02128a0afa351bf6cf8005a41f13375b22
MartinFrank77 <martin.frank@irix.ch>
2026-07-29 08:04:57 +0200

    added a new adventure

---
+++ src/test/java/com/github/martinfrank/elitegames/llmrpgengine/adventure/Verfuehrung.java
@@ -0,0 +1,97 @@
+package com.github.martinfrank.elitegames.llmrpgengine.adventure;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Verfuehrung implements Adventure {
+
+    @Override
+    public String getPlotSummary() {
+        return "";
+    }
+
+    @Override
+    public Intro getIntro() {
+        return null;
+    }
+
+    @Override
+    public List<Chapter> getChapters() {
+        return List.of();
+    }
+
+    @Override
+    public List<Person> getPersons() {
+        return List.of();
+    }
+
+    @Override
+    public List<Item> getItems() {
+        return List.of();
+    }
+
+    @Override
+    public List<Dialog> getDialogs() {
+        return List.of();
+    }
+
+    @Override
+    public List<Location> getLocations() {
+        return List.of();
+    }
+
+    @Override
+    public List<Condition<?>> getConditions() {
+        return List.of();
+    }
+
+    @Override
+    public List<Flag<?>> getFlags() {
+        return List.of();
+    }
+
+    @Override
+    public List<Knowledge> getKnowledges() {
+        return List.of();
+    }
+
+    @Override
+    public List<KnowledgeTrigger> getKnowledgeTriggers() {
+        return List.of();
+    }
+
+    @Override
+    public Condition<?> getCondition(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Location getLocation(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Person getPerson(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Flag<?> getFlag(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Dialog getDialog(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Knowledge getKnowledge(UUID id) {
+        return null;
+    }
+
+    @Override
+    public KnowledgeTrigger getKnowledgeTrigger(UUID id) {
+        return null;
+    }
+}
