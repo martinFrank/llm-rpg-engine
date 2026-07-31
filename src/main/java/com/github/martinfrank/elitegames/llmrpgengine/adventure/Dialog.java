1bd7ff6f2332b1539e8a4d8242846c570be6cc7c
MartinFrank77 <martin.frank@irix.ch>
2026-07-31 08:00:41 +0200

    added gossip

--- src/main/java/com/github/martinfrank/elitegames/llmrpgengine/adventure/Dialog.java
+++ src/main/java/com/github/martinfrank/elitegames/llmrpgengine/adventure/Dialog.java
@@ -5,4 +5,16 @@ import java.util.UUID;
 
 public record Dialog(UUID id, String topic, String summary, String context, List<KnowledgeTrigger> knowledgeTriggers) implements Identifiable {
 
+    public static final Dialog GOSSIP = new Dialog(
+        UUID.fromString("094cea8c-afc8-4e09-a670-3b52f7d38607"),
+            "Small talk",
+            "Belangslose Themen",
+            """
+                    Dieser Dialog wird verwendet, wenn sonst kein besonderes Thema im Vordergrund
+                    steht. Hier kann man einfach die Gesprächshistorie verwenden, um den Dialog
+                    lebendig zu erhalten.
+                    """,
+            List.of()
+    );
+
 }
